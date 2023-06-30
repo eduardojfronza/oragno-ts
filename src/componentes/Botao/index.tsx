6b3b7e82165092d9238b473a30109ed67b1b6217
unknown <edujfronza@gmail.com>
2023-06-29 21:30:30 -0300

    update READEME.mdt

--- src/componentes/Botao/index.tsx
+++ src/componentes/Botao/index.tsx
@@ -1,6 +1,13 @@
+import { ReactElement } from 'react'
 import './Botao.css'
 
-const Botao = (props) => {
+import React from 'react';
+
+interface BotaoProps{
+    children: ReactElement
+}
+
+const Botao = (props: BotaoProps) => {
     return (<button className='botao'>
         {props.children}
     </button>)
